187623f10e738a1fc6a776f4f88ff28a5529f684
unknown <austinscottwebb@icloud.com>
2022-06-24 00:31:55 -0400

    getting close

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
+var btns = document.querySelectorAll('.box');
 
-var btn = document.querySelectorAll('.box');
+
+for (i = 0; i < btns.length; ++i) {
+  btns[i].addEventListener('click', showColor);
+}
+
+
 var i;
-var match = [];
 var matchColor = [];
+var match = [];
+var youLose;
 
-//Event Listeners to respond to a click on the squares.
-for (i = 0; i < btn.length; ++i) {
- btn[i].addEventListener('click', showColor);
+
+function showColor() {
+  this.style.backgroundColor = this.getAttribute('data-color');
+  matchColor.push(this.getAttribute('data-color'));
+  match.push(this);
+  if (match.length === 2) {
+   checkMatch(); 
+  }
 }
 
 
-function showColor(){
-	this.style.backgroundColor = this.getAttribute('data-color');
-	match.push(this.getAttribute('data-color'));
-	console.log(match);
-	if(match.length === 2){
-		checkMatch();
-		match = [];
-        if (match.length === 2) {
-            checkMatch();
-	}
-}
-
-function checkMatch(){
-	if (matchColor[0] == matchColor[1]) {
-        console.log("Match");
-        setTimeout(function () {
-          match[0].style.backgroundColor = 'white';
-          match[1].style.backgroundColor = 'white';
-        }, 500);
-
-      } else {
-        console.log("Not a match");
-        setTimeout(function() {
-          match[0].style.backgroundColor = 'black';
-          match[1].style.backgroundColor = 'black';
+function checkMatch() {
+  if (matchColor[0] === matchColor[1]) {
+  
+    setTimeout(function () {
+      match[0].style.backgroundColor = 'orange';
+      match[1].style.backgroundColor = 'orange'; 
+      match[0].style.border = 'orange';
+      match[1].style.border = 'orange';
+    }, 400);
     
-        }, 500);
-      }
-      setTimeout(function() {
-        match = [];
-        matchColor = [];
-      }, 600);
+} else {
+  setTimeout(function () {
+    match[0].style.backgroundColor = 'white';
+    match[1].style.backgroundColor = 'white';
+  }, 400);
 }
-}
+
+  setTimeout(function (){
+    matchColor = [];
+    match=[];
+   
+}, 400);}
+
+
+ var youLose = setTimeout(Alert, 25000);
+
+function Alert() {
+  alert("Game Over. Please Refresh The Page!");
+ }
